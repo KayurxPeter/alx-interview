6561e2a4eceb57d1e08e854a9c51556ca02ef8db
KayurxPeter <kayurxpeterwise2015@gmail.com>
2023-08-30 14:56:17 -0700

    0-main.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,34 +1,19 @@
-
-
 #!/usr/bin/python3
-'''
-contains island perimeter solution
-'''
+"""
+Island Perimeter:
+        returns the perimeter of the island described in grid
+        """
 
 
-def island_perimeter(grid):
-    '''
-    island perimeter solution
-    Reasoning: find the perimeter of all island squares, then
-    subtract the sides that touch each other from the total perimeter
-    If a side is shared between two squares, then we'll need to
-    subtract 2 * side since each one of the two squares has that side
-    bordering the neighbouring square
-    '''
-    if not grid or any(type(arr) != list for arr in grid):
-        return 0
-    if any(len(arr) != len(grid[0]) for arr in grid):
-        return 0
-    
-    squares = connections = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col]:
-                squares += 1
-                # is there any neighbour to the right?
-                if col < len(grid[0]) - 1 and grid[row][col + 1]:
-                    connections += 1
-                    # is there any neighbour beneath the current square?
-                    if row < len(grid) - 1 and grid[row + 1][col]:
-                        connections += 1
-                        return squares * 4 - 2 * connections
+        def island_perimeter(grid):
+                """island perimenter function"""
+                    perimeter = 0
+                        for i in range(len(grid)):
+                                    for j in range(len(grid[i])):
+                                                    if grid[i][j] == 1:
+                                                                        perimeter += 4
+                                                                                        if i > 0 and grid[i-1][j] == 1:
+                                                                                                                perimeter -= 2
+                                                                                                                                if j > 0 and grid[i][j-1] == 1:
+                                                                                                                                                        perimeter -= 2
+                                                                                                                                                            return perimeter
