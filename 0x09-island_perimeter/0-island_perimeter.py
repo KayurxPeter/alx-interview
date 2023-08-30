f9aecdaeede35db3520ee4377a9762f97b39f165
KayurxPeter <kayurxpeterwise2015@gmail.com>
2023-08-30 14:29:13 -0700

    0-main.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-""" Module that contains the island_perimeter function """
+"""
+Island Perimeter:
+returns the perimeter of the island described in grid
+"""
 
 
 def island_perimeter(grid):
-        """ Function that returns the perimeter of island described in grid
-        Args:
-        grid (list[list(int)]): An island
-
-        Returns:
-        perimeter (int): Perimeter of the island.
-
-        """
-        perimeter = 0
-
-        if grid != []:
-            nColumns = len(grid[0])
-            nRows = len(grid)
-
-            for a in range(nRows):
-                for b in range(nColumns):
-                    if grid[a][b] == 1:
-                        if (a - 1) == -1 or grid[a - 1][b] == 0:
-                            perimeter += 1
-                            if (a + 1) == nRows or grid[a + 1][b] == 0:
-                                perimeter += 1
-                                if (b - 1) == -1 or grid[a][b - 1] == 0:
-                                    perimeter += 1
-                                    if (b + 1) == nColumns or grid[a][b + 1] == 0:
-                                        perimeter += 1
-
-                                        return perimeter
+    """island perimenter function"""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                    if j > 0 and grid[i][j-1] == 1:
+                        perimeter -= 2
+                        return perimeter
